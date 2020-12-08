43a736dd317594b251fd628cf2214262556a9c36
edwinnduti <nduti316@gmail.com>
2020-12-08 15:36:33 +0300

    removed space

--- main.go
+++ main.go
@@ -199,7 +199,7 @@ func CreateConnection() (*mongo.Client,error){
 	// connect to mongodb
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	
+
 	MongoURI := "mongodb://localhost:27017"
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
 		MongoURI,
